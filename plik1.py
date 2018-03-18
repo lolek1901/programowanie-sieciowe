a0bdd8a9d4c5d87bc91e0d528d690a0284b232aa
Zacniewski Artur <tcelkat18@gmail.com>
2018-03-18 12:29:13 +0100

    Kopia 18.03.2018

---
+++ plik1.py
@@ -0,0 +1,15 @@
+﻿print("Teks")
+# Tworzenie funkcji
+
+def napis():
+    print("Test funkcji")
+	
+napis()
+
+if 7>5:
+    print("7 jest wieksze od5")
+    print("druga linia")
+	
+for i in range (0, 10):
+    print(i)
+
